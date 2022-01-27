9f65050c50ca388f1ce5146e1ff385667a5bbbf2
author <26780385+Sigma582@users.noreply.github.com>
2022-01-27 04:21:48 +0100

    simplified startup

--- EasyBackUp/Program.cs
+++ EasyBackUp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace EasyBackUp
@@ -9,44 +7,6 @@ namespace EasyBackUp
     public class Program
     {
         public static async Task Main(string[] args)
-        {
-            AppDomain.CurrentDomain.UnhandledException += (o, args) => { };
-            
-            var parameters = new Parameters(args);
-            
-            if (Environment.UserInteractive)
-            {
-                await RunConsole(parameters);
-            }
-            else
-            {
-                await RunService(parameters);
-            }
-        }
-
-        private static async Task RunConsole(Parameters parameters)
-        {
-            Console.WriteLine("Running console");
-
-            var service = new BackupService();
-            var cancellationToken = new CancellationToken();
-            service.StartAsync(cancellationToken);
-
-            Console.WriteLine("Service started");
-            Console.WriteLine("Press Esc to stop");
-
-            ConsoleKeyInfo key = default;
-            while (key.Key != ConsoleKey.Escape)
-            {
-                key = Console.ReadKey(true);
-            }
-
-            await service.StopAsync(new CancellationToken());
-            Console.WriteLine("Stopped");
-            Console.ReadLine();
-        }
-
-        private static async Task RunService(Parameters parameters)
         {
             var host = Host.CreateDefaultBuilder(new string[] { })
                 .UseWindowsService(options =>
